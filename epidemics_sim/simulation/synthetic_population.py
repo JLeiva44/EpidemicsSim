37bdc27af6fc0628a1ef46c547e7347090269c29
joseLeiva <jseleivaf686@gmail.com>
2025-01-20 10:38:12 -0500

    add_serialization_to_population

--- epidemics_sim/simulation/synthetic_population.py
+++ epidemics_sim/simulation/synthetic_population.py
@@ -1,3 +1,4 @@
+import pickle  # Para serialización y deserialización
 from epidemics_sim.agents.human_agent import HumanAgent
 import random
 
@@ -13,8 +14,6 @@ size_mapping = {
     "4_personas": 4,
     "5_personas_o_mas": get_large_household_size
 }
-
-
 
 class SyntheticPopulationGenerator:
     def __init__(self, demographics):
@@ -32,15 +31,33 @@ class SyntheticPopulationGenerator:
         :return: A list of agents.
         """
         agents = self._generate_agents()
-        self._assign_households(agents)
+        #self._assign_households(agents)
+        return agents
+
+    def save_population(self, agents, filepath):
+        """
+        Serialize and save the population to a file.
+
+        :param agents: List of agents to save.
+        :param filepath: Path to the file where the population will be saved.
+        """
+        with open(filepath, 'wb') as file:
+            pickle.dump(agents, file)
+        print(f"Population saved to {filepath}.")
+
+    def load_population(self, filepath):
+        """
+        Load a population from a serialized file.
+
+        :param filepath: Path to the file where the population is saved.
+        :return: A list of agents.
+        """
+        with open(filepath, 'rb') as file:
+            agents = pickle.load(file)
+        print(f"Population loaded from {filepath}.")
         return agents
 
     def _generate_agents(self):
-        """
-        Generate agents with attributes based on demographics.
-
-        :return: A list of agents.
-        """
         agents = []
         for municipio, data in self.demographics["municipios"].items():
             num_agents = data["poblacion_total"]
@@ -56,15 +73,7 @@ class SyntheticPopulationGenerator:
                 agents.append(agent)
         return agents
 
-    
-    
-
     def _assign_households(self, agents):
-        """
-        Assign agents to households based on municipal data, ensuring each household has at least one adult.
-
-        :param agents: List of agents.
-        """
         for municipio, data in self.demographics["municipios"].items():
             municipio_agents = [agent for agent in agents if agent.municipio == municipio]
             adult = [agent for agent in municipio_agents if agent.age >= 18]
@@ -72,7 +81,6 @@ class SyntheticPopulationGenerator:
             unassigned_agents = municipio_agents.copy()
 
             household_sizes = list(data["hogares_por_tamano"].keys())
-            print(household_sizes)
             size_probabilities = list(data["hogares_por_tamano"].values())
 
             previous_length = len(unassigned_agents)
@@ -80,49 +88,28 @@ class SyntheticPopulationGenerator:
                 size_key = random.choices(household_sizes, size_probabilities)[0]
                 size = size_mapping[size_key] if isinstance(size_mapping[size_key], int) else size_mapping[size_key]()
                 size = min(size, len(unassigned_agents))
-                print(f'size key es : {size_key}')
-                print(f'size es : {size}')
 
-                # Crear hogar inicial
                 household = unassigned_agents[:size]
                 unassigned_agents = unassigned_agents[size:]
 
-                # Verificar si el hogar tiene al menos un adulto
                 if not any(agent.age >= 18 for agent in household):
                     adults = [agent for agent in unassigned_agents if agent.age >= 18]
                     if adults:
-                        # Reemplazar el último miembro del hogar con un adulto disponible
                         adult = adults.pop(0)
                         household[-1] = adult
                         unassigned_agents.remove(adult)
-                        print("Asignando un adulto")
-                    else:
-                        print("Advertencia: No hay suficientes adultos disponibles para asignar.")
-                        break  # Romper el ciclo si no se puede asignar un adulto
 
-                # Asignar ID único al hogar
                 household_id = household[0].agent_id
                 for agent in household:
                     agent.household_id = household_id
 
                 households.append(household)
 
-                # Condición de ruptura adicional
                 if len(unassigned_agents) == previous_length:
-                    print("Ciclo completado o error detectado.")
                     break
                 previous_length = len(unassigned_agents)
 
-
     def _generate_comorbidities(self, age, gender, municipio):
-        """
-        Generate comorbidities based on the demographics distribution for the agent's municipio.
-
-        :param age: Age of the agent.
-        :param gender: Gender of the agent.
-        :param municipio: Municipality of the agent.
-        :return: A dictionary of comorbidities for an agent.
-        """
         municipio_data = self.demographics["municipios"][municipio]
         comorbidities = {}
 
@@ -132,12 +119,6 @@ class SyntheticPopulationGenerator:
         return comorbidities
 
     def _generate_occupation(self, age):
-        """
-        Generate an occupation based on the agent's age.
-
-        :param age: Age of the agent.
-        :return: The occupation of the agent.
-        """
         if age < 18:
             return "student"
         elif age < 65:
@@ -146,12 +127,6 @@ class SyntheticPopulationGenerator:
             return "retired"
 
     def _generate_age(self, age_distribution):
-        """
-        Generate an age based on the age distribution for a municipio.
-
-        :param age_distribution: Dictionary of age ranges and their probabilities.
-        :return: An integer representing the age of an agent.
-        """
         ranges = list(age_distribution.keys())
         probabilities = list(age_distribution.values())
 
@@ -164,10 +139,4 @@ class SyntheticPopulationGenerator:
             return random.randint(65, 100)
 
     def _generate_gender(self):
-        """
-        Generate a gender based on a 50/50 distribution.
-
-        :return: A string representing the gender of an agent.
-        """
         return random.choice(["male", "female"])
-
